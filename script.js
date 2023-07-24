cc9810fd345c5f3c3a6d8d57eac9a2dcddaab300
Josue <jos.rt126@gmail.com>
2023-07-24 04:08:42 -0700

    ActualJavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+//esta funcion debe estar dentro de la variable encriptar
+function encriptar(stringEncriptado){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncriptado=stringEncriptado.toLowerCase()
+    //iniciar de cero, debe ser menor que nestra matriz codigo y length es la matriz, para despues incrementar 
+    for(let i = 0; i<matrizCodigo.length; i++){
+        if(stringEncriptado.includes(matrizCodigo[i][0])){
+            stringEncriptado=stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+
